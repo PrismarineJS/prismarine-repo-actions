a498a43a6b6ac2b4cdb0bc5ed6c467899f6c03df
extremeheat <extreme@protonmail.ch>
2023-06-27 07:49:38 +0000

    debugging

--- src/github.js
+++ src/github.js
@@ -14,12 +14,13 @@ if (globalThis.isMocha || !process.env.GITHUB_REPOSITORY) {
 
 // const { Octokit } = require('@octokit/rest') // https://github.com/octokit/rest.js
 const github = require('@actions/github')
-const core = require('@actions/core');
-
-const token = process.env.GITHUB_TOKEN || core.getInput('token')
-if (!token) throw new Error('No Github API token was specified, please see the documentation for correct Action usage.')
-const octokit = github.getOctokit(token)
 const context = github.context
+
+console.log('CONTEXT', context)
+
+const token = process.env.GITHUB_TOKEN
+if (!token) throw new Error('No Github token was specified, please see the documentation for correct Action usage.')
+const octokit = github.getOctokit(token)
 
 async function getIssueStatus (title) {
   // https://docs.github.com/en/rest/reference/search#search-issues-and-pull-requests
