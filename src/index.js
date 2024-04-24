be85705f31aa10c20d86418061a4119746618a1a
extremeheat <extreme@protonmail.ch>
2024-04-24 12:18:36 -0400

    Add config options, update readme

--- src/index.js
+++ src/index.js
@@ -255,17 +255,19 @@ const commands = {
    * @this {import('gh-helpers').HookOnRepoCommentPayload}
    */
   async mergeonpass ([], sargs) {
-    // wait for upto 10 minutes for the checks to pass
-    const MAX_WAIT = 10 * 60 * 1000
+    // wait for upto 10 minutes for the checks to pass by default
+    const MAX_WAIT = github.getInput('/mergeonpass.maxWaitTime') || 10 * 60 * 1000
+    const DEFAULT_RETRIES = github.getInput('/mergeonpass.defaultRetries') || 1
+    const DEFAULT_MODE = github.getInput('/mergeonpass.defaultMode') || 'squash'
     if (this.type !== 'pull') return
-    let retries = 0
+    let retries = DEFAULT_RETRIES
     /** @type {'squash' | 'merge' | 'rebase'} */
-    let mode = 'squash'
+    let mode = DEFAULT_MODE
     const [first, message] = sargs.split('\n')
     if (first) {
       const [_retries, _mode] = first.split(' ')
-      retries = parseInt(_retries) || 0
-      mode = _mode || 'squash'
+      retries = parseInt(_retries) || retries
+      mode = _mode || mode
       if (!['squash', 'merge', 'rebase'].includes(mode)) {
         return raise('Invalid merge mode, must be one of { squash, merge, rebase } set')
       }
@@ -273,15 +275,21 @@ const commands = {
     const prInfo = await github.getPullRequest(this.issue.number)
     const checks = await github.getPullRequestChecks(prInfo.number)
     console.log('PR Checks', checks)
+    if (!checks.length) {
+      // No checks found, sometimes the checks are not yet started. So wait a bit before below code runs.
+      await new Promise(resolve => setTimeout(resolve, 5000))
+    }
 
     do {
       const waited = await github.waitForPullRequestChecks(prInfo.number, MAX_WAIT)
       console.log('Waited for checks', waited)
       if (waited.every(e => e.conclusion === 'success')) {
         return await github.mergePullRequest(prInfo.number, { method: mode, title: message })
-      } else {
+      } else if (waited.some(e => e.conclusion === 'failure')) {
         await github.retryPullRequestChecks(prInfo.number)
         console.log('Retrying checks', retries)
+      } else {
+        return raise(`Sorry, I couldn't merge the PR because some checks are in an unknown state.\n<pre>${JSON.stringify(waited, null, 2)}</pre>`)
       }
     } while (retries-- > 0)
 
