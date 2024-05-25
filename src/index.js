c61147f07d8ec6b530d0936be8a0539d9d440068
extremeheat <extreme@protonmail.ch>
2024-05-25 17:12:01 +0000

    test

--- src/index.js
+++ src/index.js
@@ -164,7 +164,7 @@ const commands = {
     } else {
       console.log('PR found', prInfo)
     }
-    exec('git credential-cache exit')
+    exec('git credential reject')
     exec(`git remote add fork ${prInfo.getHeadClonePatURL().replace('https://', 'https://a')}`)
     exec(`git remote -v`)
     exec(`git config --list --show-origin`)
